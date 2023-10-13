fe3d812de8d31c7476ce39a9eaecb8394459d616
nanookOlive <olivier.stierer@gmail.com>
2023-10-13 22:01:03 +0200

    design chatFrame

--- chatFrame.php
+++ chatFrame.php
@@ -19,23 +19,35 @@ if(isset($_POST['pseudo'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='app.css'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&display=swap" rel="stylesheet">
     <title>Be Nasty</title>
 </head>
-<body>
-    <div class='welcome'>Welcome <?=$pseudo?></div>
-    <div id='status'></div>
-    <div class='chat-container'>
-        
-        <div id='frame'></div>
-        <div id='member-connected'>
-            <h3>membres connectés </h3>
-            <ul id='liste'></ul>
+<body class='chat-body'>
+
+    <div class='welcome'>Welcome <?=$pseudo?> ! <div id='status'></div></div>
+    <div class='chat-mamaFlex'>
+        <div class='chat-container-left'>
+
+            <img src='catcher_left.jpg' class='picture-catcher'>
+            
+            <div id='member-connected'>
+                <h3>Nasty bears connected</h3>
+                <ul id='liste'></ul>
+            </div>
+
+        </div>    
+
+        <div class='chat-container-right'>
+            <div id='frame'></div>
+            <div class='button-input'>
+                <input type='text' id='content'>
+                <input type='submit' value='Yell' id='submit'>
+                <button type='button' id='deco'>Rage Quit</button>
+            </div>
         </div>
     </div>
-    <input type='text' id='content'>
-    <input type='submit' value='Yell' id='submit'>
-    <button type='button' id='deco'>Rage Quit</button>
-    
 </body>
 <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 <script>
@@ -45,7 +57,7 @@ var connection = new WebSocket('ws://localhost:8080?pseudo=<?=$pseudo?>');
  
 connection.onopen=function(event){
 
-    $('#status').append('you\'re connected ... go ahead !!!');
+    $('#status').append(' You\'re connected ... go ahead !!!');
   
    
  }
@@ -65,7 +77,7 @@ connection.onmessage=function(event){
         });
     }
     else{
-        $('#frame').append("<p>"+user+" dit : "+content+"</p>");
+        $('#frame').append("<p class='message-user'>"+user+" dit : "+content+"</p>");
     }
 
     //clear input field 
